c805ecfaa8cdab2dc5e637d0b42f3929a1d0c2e8
jay_wu <jay_wu>
2021-05-20 18:59:51 +0800

    add auth_module

---
+++ lib/modules/auth/auth_module.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_modular/flutter_modular.dart';
+
+/// We recommend that you split your code in various modules, such as `AuthModule`,
+/// and place all the routes related to this module within it.
+/// By doing so, it will much easier to maintain and share your code with other projects.
+class AuthModule extends Module {
+  @override
+  final List<Bind> binds = [];
+
+  @override
+  final List<ModularRoute> routes = [
+  ];
+}
