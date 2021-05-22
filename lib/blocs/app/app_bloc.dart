3102049c0a575be8007fca0a1822f578a398e51a
jay_wu <jay_wu>
2021-05-22 15:00:29 +0800

    [pkg // flutter_modular] add Disposable on app_bloc

--- lib/blocs/app/app_bloc.dart
+++ lib/blocs/app/app_bloc.dart
@@ -2,11 +2,12 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_modular/flutter_modular.dart' show Disposable;
 
 part 'app_event.dart';
 part 'app_state.dart';
 
-class AppBloc extends Bloc<AppEvent, AppState> {
+class AppBloc extends Bloc<AppEvent, AppState> implements Disposable {
   AppBloc() : super(AppInitial());
 
   @override
@@ -14,5 +15,11 @@ class AppBloc extends Bloc<AppEvent, AppState> {
     AppEvent event,
   ) async* {
     // TODO: implement mapEventToState
+  }
+
+  /// In Modular, `Disposable` classes are automatically disposed when out of the module scope.
+  @override
+  void dispose() {
+
   }
 }
