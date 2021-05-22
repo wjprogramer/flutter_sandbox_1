a11e57eba9bd2734856145fd212b5715ba189f3f
jay_wu <jay_wu>
2021-05-22 14:56:55 +0800

    [pkg // flutter_modular] dependency Injection

--- lib/modules/app/app_module.dart
+++ lib/modules/app/app_module.dart
@@ -1,14 +1,31 @@
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_sandbox_1/blocs/app/app_bloc.dart';
+import 'package:flutter_sandbox_1/models/product_model.dart';
 import 'package:flutter_sandbox_1/modules/home/home_guard.dart';
 import 'package:flutter_sandbox_1/modules/home/home_module.dart';
 import 'package:flutter_sandbox_1/pages/home_page.dart';
 import 'package:flutter_sandbox_1/pages/login_page.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+final myObject = ProductModel(id: '1', name: 'Cake', price: 12.0);
 
 class AppModule extends Module {
 
   // Provide a list of dependencies to inject into your project
+  // Ex: BLoCs, ChangeNotifier classes or stores(MobX)
   @override
-  final List<Bind> binds = [];
+  final List<Bind> binds = [
+    Bind((i) => AppBloc()),
+    // Factory: Instantiate the class whenever it gets called.
+    Bind.factory((i) => AppBloc()),
+    // Instance: Use a class that has already been instantiated.
+    Bind.instance(myObject),
+    // Singleton: Create a Global instance of a class
+    Bind.singleton((i) => AppBloc()),
+    // LazySingleton: Create a Global instance of a class only when it gets called for the first time.
+    Bind.lazySingleton((i) => AppBloc()),
+    AsyncBind((i) => SharedPreferences.getInstance())
+  ];
 
   // Provide all the routes for your module
   @override
